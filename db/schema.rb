71bee9800503cff03a5da0ef6170dc6093584cdd
Pihla Toivanen <toivanenpihla@gmail.com>
2016-02-17 15:07:40 +0200

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217043328) do
+ActiveRecord::Schema.define(version: 20160217052735) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20160217043328) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "email"
+    t.string   "phonenumber"
   end
 
 end
