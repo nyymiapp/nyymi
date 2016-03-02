cc359903d268bb69491b884da8c6319c6c37ce32
Pihla Toivanen <toivanenpihla@gmail.com>
2016-03-02 13:21:44 +0200

    lol2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    session[:user_id] = nil
     return nil if session[:user_id].nil?
     User.find(session[:user_id])
   end
